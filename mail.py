c839e740d1699de3031d3a7aad90547f1d348b85
Ford Parsons <fparsons@FordBookPro.local>
2011-03-26 00:38:32 -0500

    Added newuser@ (like HealthVault).  TODO send email to that new user.

--- mail.py
+++ mail.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import logging, email
-
 from models import *
 from utils import *
+
+import logging, email, hashlib
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -11,6 +11,15 @@ from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.api import mail
 
 # Handlers
+
+class NewUserHandler(InboundMailHandler):
+    def receive(self, mail_message):
+        new_account_email = mail_message.subject
+        new_password = key_name()
+        if new_account_email and Account.all().filter('email =', new_account_email).count() == 0:
+            account = Account(email=new_account_email, passwd=hashlib.sha1(new_password).hexdigest(), name=new_account_email)
+            account.put()
+            Profile(key_name=key_name(), parent=account, account=account, name=account.name, author=account.email).put()
 
 class MailHandler(InboundMailHandler):
     def receive(self, mail_message):
@@ -28,7 +37,8 @@ def get_mime_type(filename):
             
 def main():
     application = webapp.WSGIApplication([
-        MailHandler.mapping(),
+        ('/_ah/mail/newuser%40gdatahealth.appspotmail.com', NewUserHandler),
+        ('/_ah/mail/.+', MailHandler),
     ], debug=True)
     util.run_wsgi_app(application)
 
